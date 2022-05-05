cf4d63c6d9846433d4ad05f491b791b8e5be732d
EriqueRocha <eriquebit@gmail.com>
2022-05-05 01:15:18 -0300

    Initial commit

---
+++ app/src/main/java/co/eriquerocha/codelab/mayapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package co.eriquerocha.codelab.mayapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import co.tiagoaguiar.codelab.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+	private View btnImc;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	btnImc = findViewById(R.id.btn_imc);
+
+	btnImc.setOnClickListener(new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+			startActivity(intent);
+		}
+	});
+	}
+}
